19176d4796c611ebf36d2c68f03e94ec2060a2fe
k2gsalem <k2gsalem@gmail.com>
2020-06-25 13:08:04 +0530

    envurl

--- config/app.php
+++ config/app.php
@@ -44,7 +44,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://avsapi.hridham.com'),
     'asset_url' => env('ASSET_URL', null),
     /*
     |--------------------------------------------------------------------------
